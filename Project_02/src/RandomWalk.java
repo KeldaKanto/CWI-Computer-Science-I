6ae82017a10c36ffa2a21f4c8c950a2713ca275c
Emilia Montz <emiliamontz@outlook.com>
2025-11-24 20:23:49 -0700

    Moved working directory to root for easier access & progress with project_02

--- Project_02/src/RandomWalk.java
+++ Project_02/src/RandomWalk.java
@@ -6,7 +6,7 @@ import edu.cwi.randomwalk.RandomWalkInterface;
 
 public class RandomWalk implements RandomWalkInterface {
     int size; 
-    boolean done;
+    boolean done = false;
 
     ArrayList<Point> path = new ArrayList<>();
     Point start, end, current;
@@ -15,27 +15,44 @@ public class RandomWalk implements RandomWalkInterface {
 
     public RandomWalk(int gridSize) {
         start = new Point(0, gridSize - 1);
-        end = new Point(gridSize -1, 0);
+        end = new Point(gridSize - 1, 0);
+        current = new Point(start);
 
+        path.add(start); // make sure to add the start to the path record
         rng = new Random();
     }
 
     public RandomWalk(int gridSize, long seed) {
         start = new Point(0, gridSize - 1);
-        end = new Point(gridSize -1, 0);
+        end = new Point(gridSize - 1, 0);
+        current = new Point(start);
 
+        path.add(start); // make sure to add the start to the path record
         rng = new Random(seed);
     }
 
     public void step() {
-        Point p1 = new Point(x, y);
-        path.add(p1);
-
-
+        if (current.equals(end)) {
+            done = true;
+        } else {
+            if (current.x == end.x) { // if current point is at the edge, only move along it
+                current.translate(0, -1);
+            } else if (current.y == end.y) {
+                current.translate(1, 0);
+            } else { // move up or to the side at random
+                if (rng.nextInt(2) == 0) {
+                    current.translate(1, 0);
+                } else {
+                    current.translate(0, -1);
+                }
+            } path.add(new Point(current)); // record this location as a new point
+        }
     }
 
     public void createWalk() {
-
+        while (!isDone()) {
+            step();
+        }
     }
 
     public boolean isDone() {
@@ -43,7 +60,7 @@ public class RandomWalk implements RandomWalkInterface {
     }
 
     public int getGridSize() {
-
+        return size;
     }
 
     public Point getStartPoint() {
@@ -59,11 +76,14 @@ public class RandomWalk implements RandomWalkInterface {
     }
 
     public ArrayList<Point> getPath() {
-
+        return path;
     }
 
     @Override
     public String toString() {
-
+        StringBuilder printString = new StringBuilder();
+        for ( Point points : path ) {
+            printString.append("[").append(points.x).append(",").append(points.y).append("] ");
+        } return printString.toString();
     }
 }
